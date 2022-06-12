ec6711eaf827a3d0dfdc11cd64d6d0c6c2fb1dd5
anderson-mntn <88107035+anderson-mntn@users.noreply.github.com>
2022-06-12 20:11:31 -0300

    Add files via upload

--- Sum, objects.js
+++ Sum, objects.js
@@ -31,6 +31,9 @@ for (let i = 0; i <= arr.length; i++){
      console.log(arr)
   }
 }
+
+// ------------------------------------------------------------
+
 //pag.45 - Objects, Classes (or contructors)
 let obj = {
     name : {firstName : "Gandalf", lastName : "The Grey"},
@@ -59,6 +62,7 @@ console.log(book.title)
 book.printIsbn()
 console.log(Book)
 
+// ------------------------------------------------------------
 
 //Page 55 - var, let & const
 
@@ -84,4 +88,55 @@ function blizzFan(){
     (k <= 5 ? faction = "Alliance! " : faction = "Horde! ")
     return phrase + faction
 }
-setInterval(incK, 700)
+//setInterval(incK, 700)
+
+// ------------------------------------------------------------
+
+//Pag. 58 - Arrow Function
+//Function - r = argument
+var circleAreaES5 = function circleArea (r){
+    var PI = 3.14
+    var area = PI * r * r
+    return area
+}
+console.log(circleAreaES5(4));
+
+//Arrow Function
+const circleArea = r => {
+    const PI = 3.14
+    const area = PI * r * r
+    return area
+}
+console.log(circleArea(4))
+
+//Shorter version
+const circleArea2 = r => 3.14 * r * r;
+console.log(circleArea2(4))
+
+//No Argument = empty parentheses ()
+const hello = () => console.log("Hello!");
+hello();
+
+const calc2x = n => 2 * n;
+console.log(calc2x(18))
+
+//Pag. 59 - Default Values for parameters
+function sum2 (x=1, y=2, z=3){
+    return x + y + z
+}
+console.log(sum2(4, 5)) // z default value = 3
+// Arrays into parameters ----------------
+// Spread Operators (...params)
+//we need this cs all items from array must be Numbers
+
+let arrSum3 = [3, 2, 5]
+
+function sum3(x, y, z){
+    return x + y + z
+}
+console.log("Sum of All Numbers: " + sum3(...arrSum3))
+//Min & max values from array
+console.log("lowest number: " + Math.min(...arrSum3))
+console.log("highest number: " + Math.max(...arrSum3))
+
+
